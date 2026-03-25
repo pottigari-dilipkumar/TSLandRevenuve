fc3d31a3749b6cc99aeb189c20a48a2dafa68461
pottigari-dilipkumar <pdilukumar@gmail.com>
2026-03-25 11:00:55 +0530

    Use fully-qualified Spring bean names to prevent scan conflicts

--- app-backend/src/main/java/in/gov/landrevenue/LandRevenueApplication.java
+++ app-backend/src/main/java/in/gov/landrevenue/LandRevenueApplication.java
@@ -2,8 +2,11 @@ package in.gov.landrevenue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
 @SpringBootApplication
+@ComponentScan(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
 public class LandRevenueApplication {
     public static void main(String[] args) {
         SpringApplication.run(LandRevenueApplication.class, args);
